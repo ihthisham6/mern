a1a22c37b793f8072435f5a0d0bc5df49e40c278
ihthisham6 <126441080+ihthisham6@users.noreply.github.com>
2023-11-04 21:30:23 +0530

    Create contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact(){
+    return (
+        <div id="box1">
+        <h1>contact page</h1>
+        <p>7353642892</p>
+        {/* <img src="" alt="loading" /> */}
+        </div>
+    )
+}
+
+export default Contact;
