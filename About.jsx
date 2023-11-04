ba9b7148b0f51a6766f8d1bc46540869d152a912
ihthisham6 <126441080+ihthisham6@users.noreply.github.com>
2023-11-04 21:31:14 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About(){
+    return (
+        <div id="box3">
+        <h1>About me</h1>
+        {/* <img src="" alt="loading" /> */}
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque error quia nisi officia libero accusamus quas incidunt. Ab, rerum, asperiores itaque dicta tempora eius reprehenderit, recusandae quos commodi quas perferendis?
+        Temporibus, enim. Aliquam blanditiis ab molestias impedit soluta, placeat laudantium eum totam suscipit quos, facilis sunt nostrum recusandae repellat voluptates expedita tempora eaque distinctio nisi. Eos voluptatibus voluptate soluta reprehenderit?
+        Eveniet eligendi sed, est at impedit ipsum. Odio maxime quia amet deserunt corrupti dolorem officia adipisci, aspernatur eum, eius perspiciatis reprehenderit numquam sapiente ratione quaerat sunt eveniet hic facere! Corrupti.</p>
+        </div>
+    )
+}
+
+export default About;
