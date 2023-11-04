b3d14f8ef0344c650368eedfffcee4e4721dce35
ihthisham6 <126441080+ihthisham6@users.noreply.github.com>
2023-11-04 21:29:27 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(){
+    return (
+        <div id="box2">
+        <h1>Home Page</h1>
+        <img src="https://th.bing.com/th/id/OIG.rkfXTw9GpS2h18LZ3pqg?pid=ImgGn" alt="loading" />
+        </div>
+    )
+}
+
+export default Home;
